611fce945dbf29b744ebcd1a042744ebef641431
Ka Wai HO <woodyho13@gmail.com>
2022-06-08 23:45:13 -0500

    Test

--- src/integrator.jl
+++ src/integrator.jl
@@ -1,7 +1,7 @@
 # ----------
 # Time Integrator Function for MHDFlows
 # ----------
-
+#11:42pm_8June2022_Test
 function TimeIntegrator!(prob,t₀ :: Number,N₀ :: Int;
                                        usr_dt = 0.0,
                                         diags = [],

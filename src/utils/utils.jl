7f7ddb0ccdc88ef79911bd4094e3513fb24c72f4
Ka Wai HO <woodyho13@gmail.com>
2022-11-08 17:11:28 -0600

    pre  v0.1.4 Update

--- src/utils/utils.jl
+++ src/utils/utils.jl
@@ -12,8 +12,13 @@ end
 Base.eltype(grid::SimpleGrid) = eltype(grid.k);
 
 function GetSimpleThreeDGrid(nx = 64, Lx = 2π, ny = nx, Ly = Lx, nz = nx, Lz = Lx;
+<<<<<<< HEAD
                              nthreads=Sys.CPU_THREADS, effort=FFTW.MEASURE,
                              T=Float64, ArrayType=Array)
+=======
+                             nthreads=Threads.nthreads(), effort=FFTW.MEASURE,
+                             T=Float64, ArrayType=Array,dev=CPU())
+>>>>>>> cd53c86 (pre  v0.1.4 Update)
   nk = nx
   nl = ny
   nm = nz

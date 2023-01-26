35bee7fed565de16af660cd700cfd6112133bbb7
woodyho13@gmail.com <HOphy!19>
2023-01-26 16:45:20 -0600

    RK3 debug

--- src/Solver/ShearingBox.jl
+++ src/Solver/ShearingBox.jl
@@ -22,62 +22,63 @@ using
 
 using LinearAlgebra: mul!, ldiv!
 
-include("MHDSolver.jl");
-include("HDSolver.jl");
-HDUᵢUpdate! = MHDSolver.UᵢUpdate!;
-MHDUᵢUpdate! = MHDSolver.UᵢUpdate!;
-BᵢUpdate! = MHDSolver.BᵢUpdate!;
+include("MHDSolver.jl")
+include("HDSolver.jl")
+HDUᵢUpdate! = MHDSolver.UᵢUpdate!
+MHDUᵢUpdate! = MHDSolver.UᵢUpdate!
+BᵢUpdate! = MHDSolver.BᵢUpdate!
 
 function Shearing_remapping!(prob)
-  Shearing_remapping!(prob.sol, prob.clock, prob.vars, prob.params, prob.grid);
+  Shearing_remapping!(prob.sol, prob.clock, prob.vars, prob.params, prob.grid)
   return nothing
 end
 
 function Shearing_coordinate_update!(N, sol, t, clock, vars, params, grid)
-  q  = params.usr_params.q;
-  τΩ = params.usr_params.τΩ;
-  Lx,Ly = grid.Lx,grid.Ly;
+  q  = params.usr_params.q
+  τΩ = params.usr_params.τΩ
+  Lx,Ly = grid.Lx,grid.Ly
   
   #Shear time interval in sub-time-step
-  dτ = clock.t - t;
+  dτ = clock.t - t
   
-  kx,ky,kz = grid.kr,grid.l,grid.m;
-  k²,k⁻²   = grid.Krsq,grid.invKrsq;
-  ky₀      = params.usr_params.ky₀;
-  τ        = params.usr_params.τ;
+  kx,ky,kz = grid.kr,grid.l,grid.m
+  k²,k⁻²   = grid.Krsq,grid.invKrsq
+  ky₀      = params.usr_params.ky₀
+  τ        = params.usr_params.τ
   
   # Construct the new shear coordinate
-  @. ky  = ky₀ + q*(τ+dτ)*kx;
-  @. k²  = kx^2 + ky^2 + kz^2;
-  @. k⁻² = 1/k²;
-  @views @. k⁻²[k².== 0] .= 0;
+  @. ky  = ky₀ + q*(τ+dτ)*kx
+  @. k²  = kx^2 + ky^2 + kz^2
+  @. k⁻² = 1/k²
+  @views @. k⁻²[k².== 0] .= 0
 
   return nothing
 end
 
 function Shearing_remapping!(sol, clock, vars, params, grid)
   t  = clock.t
-  q  = params.usr_params.q;
-  τΩ = params.usr_params.τΩ;
-  Lx,Ly = grid.Lx,grid.Ly;
+  q  = params.usr_params.q
+  τΩ = params.usr_params.τΩ
+  Lx,Ly = grid.Lx,grid.Ly
   
   #increase the shear time
-  params.usr_params.τ += clock.dt;
+  params.usr_params.τ += clock.dt
   
   # correct the shear after every shear period
   if params.usr_params.τ >= τΩ && clock.step > 1
     Field_remapping!(sol, clock, vars, params, grid)
     params.usr_params.τ = 0
   end   
-  return nothing;
+  return nothing
 end
 
 function Field_remapping!(sol, clock, vars, params, grid)
   T  = eltype(grid)
-  q  = params.usr_params.q;
-  τΩ = params.usr_params.τΩ;
+  q  = params.usr_params.q
+  τΩ = params.usr_params.τΩ
   kx,ky0 = grid.kr, grid.l1D
-  tmp = params.usr_params.tmp;
+  Lx     = grid.Lx
+  tmp = params.usr_params.tmp
     
   kymin,kymax = minimum(ky0),maximum(ky0)
   
@@ -90,36 +91,36 @@ function Field_remapping!(sol, clock, vars, params, grid)
     fieldᵢ = (@view sol[:,:,:,n])::CuArray{Complex{T},3} 
       tmpᵢ = (@view tmp[:,:,:,n])::CuArray{Complex{T},3}
     @cuda blocks = blocks threads = threads Field_remapping_CUDA!(tmpᵢ, fieldᵢ,
-                                                                  q, τΩ, kx, ky0,kymin, kymax)
+                                                                  q, τΩ, kx, ky0, kymin, kymax, Lx)
   end
 
   # Copy the data from tmp array to sol
-  copyto!(sol,tmp);
-  @. tmp*=0;
+  copyto!(sol,tmp)
+  @. tmp*=0
     
-  return nothing;
+  return nothing
 end
 
 function MHD_ShearingUpdate!(N, sol, t, clock, vars, params, grid)
-  U₀xh = params.usr_params.U₀xh;
-  U₀yh = params.usr_params.U₀yh;
-  U₀x  = params.usr_params.U₀x;
-  U₀y  = params.usr_params.U₀y;
-  q    = params.usr_params.q;
+  U₀xh = params.usr_params.U₀xh
+  U₀yh = params.usr_params.U₀yh
+  U₀x  = params.usr_params.U₀x
+  U₀y  = params.usr_params.U₀y
+  q    = params.usr_params.q
   
-  ux_ind,uy_ind = params.ux_ind,params.uy_ind;
-  bx_ind,by_ind = params.bx_ind,params.by_ind;
+  ux_ind,uy_ind = params.ux_ind,params.uy_ind
+  bx_ind,by_ind = params.bx_ind,params.by_ind
 #    exp_terms(iux) = nl(iux) + fux - zi*kxt*p + 2.d0*shear_flg*uy
 #    exp_terms(iuy) = nl(iuy) + fuy - zi*ky *p - (2.d0 - q)*shear_flg*ux  
 #    exp_terms(iby) = nl(iby) - q*shear_flg*bx
   @. N[:,:,:,ux_ind] +=  -(2 - q)*N[:,:,:,uy_ind]
   @. N[:,:,:,uy_ind] +=  +   2   *N[:,:,:,ux_ind]
   @. N[:,:,:,bx_ind] +=  -   q   *N[:,:,:,by_ind]
-  return nothing;
+  return nothing
 end
 
 function Field_remapping_CUDA!(tmpᵢ, fieldᵢ,
-                               q, τΩ, kx, ky0, kymin, kymax)
+                               q, τΩ, kx, ky0, kymin, kymax, Lx)
   #define the i,j,k
   i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
   j = (blockIdx().y - 1) * blockDim().y + threadIdx().y
@@ -127,7 +128,7 @@ function Field_remapping_CUDA!(tmpᵢ, fieldᵢ,
   nx,ny,nz = size(fieldᵢ)
   nky = length(ky0)
   if k ∈ (1:nz) && j ∈ (1:ny) && i ∈ (1:nx)
-    dky   = floor(q*τΩ*kx[i])
+    dky   = floor(q*τΩ*kx[i]*)
     kynew = ky0[j] + dky
     if  kymax >= kynew >= kymin
       mindky = 100   
@@ -148,28 +149,28 @@ function MHD_ShearingAdvection!(N, sol, t, clock, vars, params, grid)
     
   #Update V + B Real Conponment
   #@timeit_debug params.debugTimer "FFT Update" CUDA.@sync begin
-    ldiv!(vars.ux, grid.rfftplan, deepcopy(@view sol[:, :, :, params.ux_ind]));
-    ldiv!(vars.uy, grid.rfftplan, deepcopy(@view sol[:, :, :, params.uy_ind]));
-    ldiv!(vars.uz, grid.rfftplan, deepcopy(@view sol[:, :, :, params.uz_ind]));
-    ldiv!(vars.bx, grid.rfftplan, deepcopy(@view sol[:, :, :, params.bx_ind]));
-    ldiv!(vars.by, grid.rfftplan, deepcopy(@view sol[:, :, :, params.by_ind]));
-    ldiv!(vars.bz, grid.rfftplan, deepcopy(@view sol[:, :, :, params.bz_ind])); 
+    ldiv!(vars.ux, grid.rfftplan, deepcopy(@view sol[:, :, :, params.ux_ind]))
+    ldiv!(vars.uy, grid.rfftplan, deepcopy(@view sol[:, :, :, params.uy_ind]))
+    ldiv!(vars.uz, grid.rfftplan, deepcopy(@view sol[:, :, :, params.uz_ind]))
+    ldiv!(vars.bx, grid.rfftplan, deepcopy(@view sol[:, :, :, params.bx_ind]))
+    ldiv!(vars.by, grid.rfftplan, deepcopy(@view sol[:, :, :, params.by_ind]))
+    ldiv!(vars.bz, grid.rfftplan, deepcopy(@view sol[:, :, :, params.bz_ind])) 
   #end
   #Update V Advection
   #@timeit_debug params.debugTimer "UᵢUpdate" CUDA.@sync begin
-    MHDUᵢUpdate!(N, sol, t, clock, vars, params, grid;direction="x");
-    MHDUᵢUpdate!(N, sol, t, clock, vars, params, grid;direction="y");
-    MHDUᵢUpdate!(N, sol, t, clock, vars, params, grid;direction="z");
+    MHDUᵢUpdate!(N, sol, t, clock, vars, params, griddirection="x")
+    MHDUᵢUpdate!(N, sol, t, clock, vars, params, griddirection="y")
+    MHDUᵢUpdate!(N, sol, t, clock, vars, params, griddirection="z")
   #end
   #Update B Advection
   #@timeit_debug params.debugTimer "BᵢUpdate" CUDA.@sync begin
-    BᵢUpdate!(N, sol, t, clock, vars, params, grid;direction="x");
-    BᵢUpdate!(N, sol, t, clock, vars, params, grid;direction="y");
-    BᵢUpdate!(N, sol, t, clock, vars, params, grid;direction="z"); 
+    BᵢUpdate!(N, sol, t, clock, vars, params, griddirection="x")
+    BᵢUpdate!(N, sol, t, clock, vars, params, griddirection="y")
+    BᵢUpdate!(N, sol, t, clock, vars, params, griddirection="z") 
   #end
 
   #@timeit_debug params.debugTimer "ShearingUpdate" CUDA.@sync begin
-    MHD_ShearingUpdate!(N, sol, t, clock, vars, params, grid);
+    MHD_ShearingUpdate!(N, sol, t, clock, vars, params, grid)
   #end
   return nothing
 end
@@ -178,19 +179,19 @@ function HD_ShearingAdvection!(N, sol, t, clock, vars, params, grid)
 
   #Update V + B Real Conponment
   @timeit_debug params.debugTimer "FFT Update" CUDA.@sync begin
-    ldiv!(vars.ux, grid.rfftplan, deepcopy(@view sol[:, :, :, params.ux_ind]));
-    ldiv!(vars.uy, grid.rfftplan, deepcopy(@view sol[:, :, :, params.uy_ind]));
-    ldiv!(vars.uz, grid.rfftplan, deepcopy(@view sol[:, :, :, params.uz_ind]));
+    ldiv!(vars.ux, grid.rfftplan, deepcopy(@view sol[:, :, :, params.ux_ind]))
+    ldiv!(vars.uy, grid.rfftplan, deepcopy(@view sol[:, :, :, params.uy_ind]))
+    ldiv!(vars.uz, grid.rfftplan, deepcopy(@view sol[:, :, :, params.uz_ind]))
   end
   #Update V Advection
   @timeit_debug params.debugTimer "UᵢUpdate" CUDA.@sync begin
-    HDUᵢUpdate!(N, sol, t, clock, vars, params, grid;direction="x");
-    HDUᵢUpdate!(N, sol, t, clock, vars, params, grid;direction="y");
-    HDUᵢUpdate!(N, sol, t, clock, vars, params, grid;direction="z");
+    HDUᵢUpdate!(N, sol, t, clock, vars, params, griddirection="x")
+    HDUᵢUpdate!(N, sol, t, clock, vars, params, griddirection="y")
+    HDUᵢUpdate!(N, sol, t, clock, vars, params, griddirection="z")
   end
 
   #@timeit_debug params.debugTimer "ShearingUpdate" CUDA.@sync begin
-  #HD_ShearingUpdate!(N, sol, t, clock, vars, params, grid);
+  #HD_ShearingUpdate!(N, sol, t, clock, vars, params, grid)
   #end
   return nothing
 end
@@ -198,9 +199,9 @@ end
 function Shearing_dealias!(fh, grid)
     @assert grid.nkr == size(fh)[1]
     # kfilter = 2/3*kmax
-    kfilter = (2/3*3*grid.nkr)^2;
-    @views @. fh[grid.Krsq.>=kfilter,:] = 0;
-    return nothing;
+    kfilter = (2/3*3*grid.nkr)^2
+    @views @. fh[grid.Krsq.>=kfilter,:] = 0
+    return nothing
 end
 
 end

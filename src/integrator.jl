9ce6c6100f345bdf847a80ec690d960ac2fe3cd0
Ka Wai HO <woodyho13@gmail.com>
2023-03-24 20:50:57 -0500

    minor bug fixed

--- src/integrator.jl
+++ src/integrator.jl
@@ -129,7 +129,7 @@ function TimeIntegrator!(prob,t₀ :: Number,N₀ :: Int;
 
       #hyper diffusion Update
       if (! prob.flag.e) &&  (! prob.flag.c)
-        prob.params.nν >  ? Implicitdiffusion!(prob) : nothing
+        prob.params.nν >  1 ? Implicitdiffusion!(prob) : nothing
       end 
 
       #User defined function

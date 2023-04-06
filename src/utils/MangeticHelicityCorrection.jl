b37bfeda27b79ee114ba65506689fb696143267f
Ka Wai HO <woodyho13@gmail.com>
2023-04-06 01:38:38 -0500

    correction scheme update

--- src/utils/MangeticHelicityCorrection.jl
+++ src/utils/MangeticHelicityCorrection.jl
@@ -3,24 +3,58 @@
 # ----------
 
 
+# im for dddd!
+
+# im for dddd!
+
+# im for dddd!
+
+
+# im for dddd!
+
+# im for dddd!
+
+
+# im for dddd!
+
+
 # function of correction the B-field to conserve the Hm
 function HmCorrection!(prob; ε = 1f-4)
   L1_err_max(A,B) =  mapreduce(x->√(x*x - y*y),max,A,B)
   # define the variables for iteration
 
+  # ---------------------------- step 1. ------------------------------------#
   # some preparation work for computing ΔH, C0, C1 D, λ₀, real of i space?
-  H  = 1;
+
+  # Compute Vector Potential & current by assuming the Coulomb gauge ∇ ⋅ A = 0
+  @. Jxh = im*(ky*bzh - kz*byh)
+  @. Jyh = im*(kz*bxh - kx*bzh)
+  @. Jzh = im*(kx*byh - ky*bxh)
+  ldiv!(Jx, grid.rfftplan, deepcopy(Jxh))  
+  ldiv!(Jy, grid.rfftplan, deepcopy(Jyh))
+  ldiv!(Jz, grid.rfftplan, deepcopy(Jzh))
+
+  @. Axh = Jxh*k⁻²
+  @. Ayh = Jyh*k⁻²
+  @. Azh = Jzh*k⁻²
+  ldiv!(Ax, grid.rfftplan, deepcopy(Axh))  
+  ldiv!(Ay, grid.rfftplan, deepcopy(Ayh))
+  ldiv!(Az, grid.rfftplan, deepcopy(Azh))
+
+  # Compute the magnetic helicity 
+  H  = (Axh,Ayh,Azh) ⋅ (Bxh,Byh,Bzh)
+
   # Real or imag space? Should be real space? but Cᵢ is imag space?
-  C₀ = 2 *square_mean(bxh,byh,bzh)
-  @. C₁ = bxh^2 + byh^2 + bzh^2 + (Jxh,Jyh,Jzh)⋅(Axh,Ayh,Azh) - C₀  
-  @. ΔH = H - H0 
+  C₀ = 2 *square_mean(bx,by,bz)
+  @. C₁ = bx^2 + by^2 + bz^2 + (Jx,Jy,Jz)⋅(Ax,Ay,Az) - C₀  
+  @. ΔH = H - H₀ # imag space
   @. λₙ = ΔH/2/(C₀ .+ C₁)
   ComputeD₀D₁!(D₀, D₁, Axh, Ayh, Azh, grid, vars)
 
-  # compute the err 
+  # ---------------------------- step 2. ------------------------------------#
+  # compute the err and iterate the λ until the err is converge 
   err = 1.0
 
-  # iterate the λ until the err is converge 
   @. λ_next = 0
   while err > ε
 
@@ -47,7 +81,10 @@ function HmCorrection!(prob; ε = 1f-4)
 
   end
 
+  # ---------------------------- step 3. ------------------------------------#
   # work out the A' = A + δA and then compute the B' = ∇ × A'
+  ComputeδB!()  
+
 
   # data movement for next iteration
   copyto!(H₀,H)
@@ -88,16 +125,16 @@ function Get_the_∇∇²∇λB_term!(∇∇²∇λBh, λ,
 end
 
 # D should be in real space...
-function ComputeD₀D₁!(D₀, D₁, Axh, Ayh, Azh, grid, vars)
+function ComputeD₀D₁!(D₀, D₁, Ax, Ay, Az, grid, vars)
   
   δ(a::Int,b::Int) = ( a == b ? 1 : 0 )
   
   D₁ᵢⱼ = vars.nonhin1
 
-  A² = (Axh,Ayh,Azh)⋅(Axh,Ayh,Azh)
+  A² = (Ax,Ay,Az)⋅(Ax,Ay,Az)
   
-  for (i,Aᵢ) ∈ zip((1,2,3),(Axh,Ayh,Azh))
-    for (j,Aⱼ) ∈ zip((1,2,3),(Axh,Ayh,Azh))
+  for (i,Aᵢ) ∈ zip((1,2,3),(Ax,Ay,Az))
+    for (j,Aⱼ) ∈ zip((1,2,3),(Ax,Ay,Az))
   
       @. D₁[i,j] = δ(i,j)*A^2 - Aᵢ*Aⱼ
       
@@ -118,8 +155,7 @@ end
 function Get_the_∂ᵢD₁ᵢⱼ∂ⱼλₙ_term!(∂ᵢD₁ᵢⱼ∂ⱼλₙh, λ, D₁ᵢⱼ, params, vars, grid)
   
   @. ∂ᵢD₁ᵢⱼ∂ⱼλₙh = 0  
-
-  @. ∂ᵢD₁ᵢⱼ∂ⱼλₙ = 0
+  @. ∂ᵢD₁ᵢⱼ∂ⱼλₙ  = 0
 
   ∂ⱼλₙh = kⱼ*λ
 
@@ -132,4 +168,27 @@ function Get_the_∂ᵢD₁ᵢⱼ∂ⱼλₙ_term!(∂ᵢD₁ᵢⱼ∂ⱼλₙh,
   mul!(∂ᵢD₁ᵢⱼ∂ⱼλₙh, grid.rfftplan, ∂ᵢD₁ᵢⱼ∂ⱼλₙ)
 
   return nothing 
+end
+
+function ComputeδB!(λ, Ax, Ay, Az, Bx, By, Bz)
+  # δA = ∇×(λA) + λB - ∇∇⁻²∇⋅λB
+  λBxh = @. λ*Bxh
+  λByh = @. λ*Byh
+  λBzh = @. λ*Bzh
+
+  # (?) Am I do this correctly?
+  ∇⁻²∇λBh = -im*(kx*λ*Bxh + ky*λ*Byh + kz*λ*Bzh)*k⁻²
+  
+  # δA = ∇×(λA) + λB - ∇∇⁻²∇⋅λB
+  @. δAxh = im*(@. ky*Azh - kz*Byh) + λ*Bxh - im*kx*∇⁻²∇λBh
+  @. δAyh = im*(@. kz*Axh - kx*Azh) + λ*Byh - im*ky*∇⁻²∇λBh
+  @. δAzh = im*(@. kx*Ayh - ky*Axh) + λ*Bzh - im*kz*∇⁻²∇λBh
+
+  # B = B + ∇×(δA)
+  @. Bxh += im*(ky*δAzh - kz*δAyh)
+  @. Byh += im*(kz*δAxh - kx*δAzh)
+  @. Bzh += im*(kx*δAyh - ky*δAxh)
+
+  return nothing
+  
 end

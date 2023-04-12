e27882a7b850719466ef9592342aa8965d1fae8b
Ka Wai HO <woodyho13@gmail.com>
2023-04-11 21:09:26 -0500

    Correction scheme update

--- src/utils/VectorCalculus.jl
+++ src/utils/VectorCalculus.jl
@@ -1,181 +1,258 @@
 # ----------
-# Vector Calculus Module, Only work on peroideric boundary!
+# Module For Magnetic helicity correction scheme, Ref : Zenati & Vishniac 2021
 # ----------
-
-"""
-    Curl(B1,B2,B3;Lx=2π)
-
-Funtion of computing ∇ × A⃗ using the fourier method
-  Keyword arguments
-=================
-- `B1/B2/B3`:  3D i/j/k vector field array 
-- `Lx/Ly/Lz`: Length Scale for the box(T type: Int)
-- `T` : Data Type of the input Array
-"""
-function Curl(B1::Array,B2::Array,B3::Array;
-              Lx = 2π, Ly = Lx, Lz = Lx,T = Float32)
-    # Wrapper for Curl Function
-    nx,ny,nz = size(B1);
-    grid = GetSimpleThreeDGrid(nx, Lx, ny, Ly, nz, Lz, T = T);
-    cB1,cB2,cB3 = Curl(B1,B2,B3,grid)
-    return cB1,cB2,cB3;
+# Note : Have to check if any sketch variables is overlaped
+# Are A computed correctly?
+mutable struct Hm_vars{Atrans,Aphys,Aphys4D}
+  λₙ  :: Aphys
+  H₀h :: Atrans
+  sk  :: Aphys4D
 end
 
-function Curl(B1,B2,B3,grid)
-    #funtion of computing ∇×Vector using the fourier method
-    # fft(∇×Vector) -> im * k × V
-    #| i j k  |
-    #| x y z  |
-    #|B1 B2 B3|
-    #
-    nx,ny,nz = size(B1);
-    dev = typeof(B1) <: Array ? CPU() : GPU();
-    T   = eltype(grid);
+function construct_function_for_struct(::Dev,nx::Int,ny::Int,nz::Int; T = Float32) where Dev
 
-    @devzeros typeof(dev) Complex{T} (div(nx,2)+1,ny,nz) B1h B2h B3h CB1h CB2h CB3h
-    @devzeros typeof(dev)         T  (         nx,ny,nz) cB1 cB2 cB3
+  @devzeros Dev Complex{T} ( div(nx,2) + 1 , ny, nz   ) H₀h
+  @devzeros Dev         T  (            nx , ny, nz   ) λₙ
+  @devzeros Dev         T  (            nx , ny, nz, 6) sk
+  Hm = Hm_vars(λₙ, H₀h, sk)
 
-    mul!(B1h, grid.rfftplan, B1); 
-    mul!(B2h, grid.rfftplan, B2); 
-    mul!(B3h, grid.rfftplan, B3);
+  return Hm, HmCorrection!
+end
+
+# function of correction the B-field to conserve the Hm
+# note: this has a time constraint for weak field case from eq. 31
+# we have a slightly different implementation from their paper
+# we start from the equation 24. 
+# ΔH - 2C₁λ + ∂ᵢD₁ᵢⱼ∂ⱼλ + B⋅∇(∇⁻²∇⋅(λB)) - 2C₀λ + ∂ᵢD₀ᵢⱼ∂ⱼλ = 0
+# Where in k-space, we define a function g,
+# g = λ + 2C₁λ + 2C₀λ - D₀ᵢⱼkᵢkⱼλ  - (ΔH + ∂ᵢD₁ᵢⱼ∂ⱼλ + B⋅∇(∇⁻²∇⋅(λB))) (k) 
+# If the equation converage, g(λₙ₊₁) = λₙ 
+# In this case, we adopt the fix point method.
+function HmCorrection!(prob; ε = 1f-10)
+  square_mean(A,B,C) = mapreduce((x,y,z)->x*x+y*y+z*z,+,A,B,C)/length(A)
+  L2_err_max(dA) =  mapreduce(x->x*x, max, dA)
+  δ(a::Int,b::Int) = ( a == b ? 1 : 0 )
     
-    kx,ky,kz = grid.kr,grid.l,grid.m; 
-    @. CB1h = im*(ky*B3h - kz*B2h);
-    @. CB2h = im*(kz*B1h - kx*B3h);
-    @. CB3h = im*(kx*B2h - ky*B1h);
+  # ---------------------------- step 0. ------------------------------------#
+  # define all the variables for iteration
+  ts   = prob.timestepper
+  sol  = prob.sol
+  grid = prob.grid
+  vars = prob.vars
+  params = prob.params
+  sk_arr1,sk_arr2 = ts.RHS₁,ts.RHS₂
+  sk_arr3,sk_arr4 = ts.RHS₃,ts.RHS₄
     
-    ldiv!(cB1, grid.rfftplan, CB1h);  
-    ldiv!(cB2, grid.rfftplan, CB2h);
-    ldiv!(cB3, grid.rfftplan, CB3h);
-    return cB1,cB2,cB3;
-end
+  @views bxh,byh,bzh = sol[:,:,:,params.bx_ind], sol[:,:,:,params.by_ind], sol[:,:,:,params.bz_ind]
+  ldiv!(vars.bx, grid.rfftplan, deepcopy(@view sol[:, :, :, params.bx_ind]))
+  ldiv!(vars.by, grid.rfftplan, deepcopy(@view sol[:, :, :, params.by_ind]))
+  ldiv!(vars.bz, grid.rfftplan, deepcopy(@view sol[:, :, :, params.bz_ind])) 
+  bx , by, bz = vars.bx, vars.by, vars.bz
 
-"""
-    Div(B1,B2,B3;Lx=2π)
+  k⁻² = grid.invKrsq
+  kx, ky, kz = grid.kr, grid.l, grid.m
 
-Funtion of computing ∇ ⋅ ⃗A⃗using the fourier method
-  Keyword arguments
-=================
-- `B1/B2/B3`:  3D i/j/k vector field array 
-- `Lx/Ly/Lz`: Length Scale for the box(T type: Int)
-- `T` : Data Type of the input Array
-"""
-function Div(B1::Array,B2::Array,B3::Array;
-             Lx = 2π, Ly = Lx, Lz = Lx,T = Float32)
-    nx,ny,nz = size(B1);
-    grid = GetSimpleThreeDGrid(nx, Lx, ny, Ly, nz, Lz, T = T);
-    cB1 = Div(B1,B2,B3,grid);
+  # define the "pointer" of sketch variables
+  # - imag space sketch variables
+  @views Axh, Ayh, Azh = sk_arr1[:,:,:,1], sk_arr1[:,:,:,2], sk_arr1[:,:,:,3]
+  @views Jxh, Jyh, Jzh = sk_arr1[:,:,:,4], sk_arr1[:,:,:,5], sk_arr1[:,:,:,6]
+  @views sk1, sk2, sk3 = sk_arr2[:,:,:,1], sk_arr2[:,:,:,2], sk_arr2[:,:,:,3] 
+  @views sk4, sk5, sk6 = sk_arr2[:,:,:,4], sk_arr2[:,:,:,5], sk_arr2[:,:,:,6]   
+  @views Hh, ΔHh       = sk_arr3[:,:,:,1], sk_arr3[:,:,:,2]
+  @views λh_next, λₙh  = sk_arr3[:,:,:,3], sk_arr3[:,:,:,4]
+  @views ∂ᵢD₁ᵢⱼ∂ⱼλₙh   = sk_arr3[:,:,:,5]
+  @views kᵢkⱼD₀ᵢⱼ, I   = sk_arr4[:,:,:,1], sk_arr4[:,:,:,2]
+  @views B∇∇⁻²∇λBh    = sk_arr4[:,:,:,3] 
+  @views Δλh, C₁λₙh    = sk_arr4[:,:,:,4], sk_arr4[:,:,:,5] 
+  H₀h = params.usr_params.H₀h
 
-    return cB1
-end
+  # - real space sketch variables
+  @views Jx, Jy, Jz = params.usr_params.sk[:,:,:,1], params.usr_params.sk[:,:,:,2], params.usr_params.sk[:,:,:,3]
+  @views Ax, Ay, Az = params.usr_params.sk[:,:,:,4], params.usr_params.sk[:,:,:,5], params.usr_params.sk[:,:,:,6]
+  Δλ = vars.nonlin1
+  λₙ = params.usr_params.λₙ
 
-function Div(B1,B2,B3,grid)
-    #funtion of computing ∇̇ ⋅ Vector using the fourier method
-    # fft(∇·Vector) -> im * k ⋅ V
-    # = im* (x*B1 + y*B2 + z*B3)
+  # ---------------------------- step 1. ------------------------------------#
+  # some preparation work for computing ΔH, C0, C1 D, λ₀
 
-    nx,ny,nz = size(B1);
-    dev = typeof(B1) <: Array ? CPU() : GPU();
-    T   = eltype(grid);
+  # Compute Vector Potential & current by assuming the Coulomb gauge ∇ ⋅ A = 0
+  @. Jxh = im*(ky*bzh - kz*byh)
+  @. Jyh = im*(kz*bxh - kx*bzh)
+  @. Jzh = im*(kx*byh - ky*bxh)
+  ldiv!(Jx, grid.rfftplan, deepcopy(Jxh))  
+  ldiv!(Jy, grid.rfftplan, deepcopy(Jyh))
+  ldiv!(Jz, grid.rfftplan, deepcopy(Jzh))
 
-    @devzeros typeof(dev) Complex{T} (div(nx,2)+1,ny,nz) B1h B2h B3h Dot
-    @devzeros typeof(dev)         T  (         nx,ny,nz) cB1
-    
-    mul!(B1h, grid.rfftplan, B1); 
-    mul!(B2h, grid.rfftplan, B2); 
-    mul!(B3h, grid.rfftplan, B3);
-
-    kx,ky,kz = grid.kr,grid.l,grid.m; 
-    @. Dot = im*(kx*B1h+ky*B2h+kz*B3h)
-    
-    ldiv!(cB1, grid.rfftplan, Dot);  
-
-    return cB1
-end
-
-function ∂i(B1::Array, direction; Lx = 2π, Ly = Lx, Lz = Lx,T = eltype(B1))
-  nx,ny,nz = size(B1);
-  dev = typeof(B1) <: Array ? CPU() : GPU();
-  grid = GetSimpleThreeDGrid(nx, Lx, ny, Ly, nz, Lz, T = T);
-  return ∂i(B1,grid, direction);
-end
-
-function ∂i(B1::Array,grid, direction)
-  # funtion of computing x/y/z-direction of ∇̇ ⋅ Vector using the fourier method
-  # fft(∂_i(Vector)) -> im * k_i ⋅ V
-
-  nx,ny,nz = size(B1);
-  dev = typeof(B1) <: Array ? CPU() : GPU();
-  T   = eltype(grid);
-
-  @devzeros typeof(dev) Complex{T} (div(nx,2)+1,ny,nz) B1h
-  @devzeros typeof(dev)         T  (         nx,ny,nz) cB1
+  @. Axh = -Jxh*k⁻²
+  @. Ayh = -Jyh*k⁻²
+  @. Azh = -Jzh*k⁻²
+  ldiv!(Ax, grid.rfftplan, deepcopy(Axh))  
+  ldiv!(Ay, grid.rfftplan, deepcopy(Ayh))
+  ldiv!(Az, grid.rfftplan, deepcopy(Azh))
   
-  mul!(B1h, grid.rfftplan, B1);
-  kx,ky,kz = grid.kr,grid.l,grid.m;
-  if  direction == "x"
-    @. B1h = im*kx*B1h
-  elseif direction == "y"
-    @. B1h = im*ky*B1h
-  elseif direction =="z"
-    @. B1h = im*kz*B1h
-  else
-    error("Wrong driection declared")
+  # C₁ = B^2 + J ⋅ A C₀
+  C₀ = 2 *square_mean(bx,by,bz)  #eq. 20
+  C₁  = @. bx^2 + by^2 + bz^2 + Jx*Ax + Jy*Ay + Jz*Az - C₀ # eq. 21
+  # Compute the magnetic helicity 
+  H  = @. Ax*bx + Ay*by + Az*bz
+  mul!(Hh, grid.rfftplan, H)
+  @. ΔHh = Hh - H₀h # imag space
+  ldiv!(λₙ, grid.rfftplan, deepcopy(ΔHh))
+  @. λₙ  = λₙ/2/C₀  # eq. 27
+  mul!(λₙh, grid.rfftplan, λₙ)
+
+
+  # ---------------------------- step 2. ------------------------------------#
+  # compute the err and iterate the λ until it is converge 
+  err = 1.0
+#=
+  @. λh_next = 0
+  @.       I = 1
+  A² = @. Ax*Ax + Ay*Ay + Az*Az
+  for i = 1:1
+
+    # compute the C₁λₙ term
+    ldiv!(λₙ, grid.rfftplan, deepcopy(λₙh))
+    C₁λₙ = @. C₁*λₙ 
+    mul!(C₁λₙh, grid.rfftplan, C₁λₙ);
+
+    # compute the  ∑_i B̂ᵢ kᵢ (k^{-2} ∑_i( kᵢ \widehat{λB} ))
+    Get_the_B∇∇⁻²∇λB_term!(B∇∇⁻²∇λBh, λₙ, 
+                            bx ,by ,bz,
+                            params, vars, grid, ∇⁻²∇λBh = sk1)
+    @.   kᵢkⱼD₀ᵢⱼ = 0   
+    @. ∂ᵢD₁ᵢⱼ∂ⱼλₙh = 0
+    # Implicit summation for computing λₙ₊₁ from eq. 25 in the paper
+    for (Aᵢ,kᵢ,i) ∈ zip((Ax,Ay,Az), (kx,ky,kz), (1,2,3))
+      for (Aⱼ,kⱼ,j) ∈ zip((Ax,Ay,Az), (kx,ky,kz), (1,2,3))
+        δᵢⱼ  = δ(i,j)
+        D₁ᵢⱼ = @. δᵢⱼ*A² - Aᵢ*Aⱼ      
+        D₀ᵢⱼ = mean(D₁ᵢⱼ)
+        @. kᵢkⱼD₀ᵢⱼ += -kᵢ*kⱼ*I*D₀ᵢⱼ     # eq. 22, "-" is coming im*im
+        D₁ᵢⱼ = @. D₁ᵢⱼ - D₀ᵢⱼ           # eq. 23
+        Get_the_∂ᵢD₁ᵢⱼ∂ⱼλₙ_term!(∂ᵢD₁ᵢⱼ∂ⱼλₙh, λₙh, D₁ᵢⱼ, 
+                                 kᵢ, kⱼ, params, vars, grid)
+      end
+    end
+    # ΔH - 2C₁λ + ∂ᵢD₁ᵢⱼ∂ⱼλ + B⋅∇(∇⁻²∇⋅(λB)) - 2C₀λ + ∂ᵢD₀ᵢⱼ∂ⱼλ = 0
+    #  λₙ₊₁ = g = λₙ + 2C₁λₙ + 2C₀λₙ - D₀ᵢⱼkᵢkⱼλₙ  - (ΔH + ∂ᵢD₁ᵢⱼ∂ⱼλₙ + B⋅∇(∇⁻²∇⋅(λₙB))) (k) 
+    @. λh_next = λₙh + ΔHh + ∂ᵢD₁ᵢⱼ∂ⱼλₙh + B∇∇⁻²∇λBh - 2*C₁λₙh - 2*C₀*λₙh + kᵢkⱼD₀ᵢⱼ*λₙh  
+   
+    # compute the rms error in real space      
+    @. Δλh = λh_next - λₙh
+    ldiv!(Δλ, grid.rfftplan, Δλh)
+    err = maximum(Δλ.^2)
+    #@show sum(isnan.(λh_next))
+    @show sqrt(err)
+    # data movement for next iteration
+    copyto!(λₙh, λh_next)
+    @. λh_next  = 0
+
   end
-  ldiv!(cB1, grid.rfftplan, B1h); 
+  =#  
+  dealias!(λₙh,grid)
+  # ---------------------------- step 3. ------------------------------------#
+  # work out the δA and then compute the B' = B₀ + ∇ × δA
+  ldiv!(λₙ, grid.rfftplan, deepcopy(λₙh)) 
+  #ComputeδB!(λₙ, Ax, Ay, Az, 
+  #           bx, by, bz, bxh, byh, bzh,
+  #           vars, params, grid; 
+  #           λBxh = sk1, λByh = sk2, λBzh = sk3,
+  #           λAxh = sk4, λAyh = sk5, λAzh = sk6,
+  #           δAxh = Axh, δAyh = Ayh, δAzh = Azh)
 
-  return cB1
+    return @. C₁
 end
 
+# compute the  ∑_i B̂ᵢ kᵢ (k^{-2} ∑_i( kᵢ \widehat{λB} ))
+function Get_the_B∇∇⁻²∇λB_term!(B∇∇⁻²∇λBh, λ, 
+                                  bx ,by ,bz,
+                                  params, vars, grid; ∇⁻²∇λBh = sk1)
 
-∇X(A1,A2,A3;Lx = 2π, Ly = Lx, Lz = Lx,T = eltype(A1)) =  Curl(A1,A2,A3;Lx = Lx, Ly = Ly, Lz = Lz,T = eltype(A1));
-#∇·(A1,A2,A3;Lx = 2π, Ly = Lx, Lz = Lx,T = eltype(A1)) =   Div(A1,A2,A3;Lx = Lx, Ly = Ly, Lz = Lz,T = eltype(A1));
+  ∂ᵢ∇⁻²∇λB  = λbᵢ = vars.nonlin1
+  ∂ᵢ∇⁻²∇λBh = Bᵢ∂ᵢ∇⁻²∇λBh = λbᵢh = vars.nonlinh1
 
+  @. ∇⁻²∇λBh  = 0 
+  @. B∇∇⁻²∇λBh = 0
 
-"""
-    LaplaceSolver(B)
+  kx, ky, kz = grid.kr, grid.l, grid.m
+  k⁻² = grid.invKrsq;
 
-Funtion of Solving ΔΦ = B using the fourier method
-  Keyword arguments
-=================
-- `B`:  3D scalar array 
-- `Lx/Ly/Lz`: Length Scale for the box(T type: Int)
-- `T` : Data Type of the input Array
-"""
-function LaplaceSolver(B; Lx=2π, Ly = Lx, Lz = Lx, T = Float32)
-    nx,ny,nz = size(B);
-    grid = GetSimpleThreeDGrid(nx, Lx, ny, Ly, nz, Lz, T = T);
-    Φ   = LaplaceSolver(B,grid);
-    return Φ
+  for (kᵢ,bᵢ) ∈ zip((kx,ky,kz),(bx,by,bz))
+    @. λbᵢ = λ*bᵢ
+    mul!(λbᵢh, grid.rfftplan, λbᵢ)
+    @. ∇⁻²∇λBh += -im*kᵢ*λbᵢh*k⁻² # note: -1 is coming  ∇⁻²
+  end
+
+  for (kᵢ,bᵢ) ∈ zip((kx,ky,kz),(bx,by,bz))
+    
+    @. ∂ᵢ∇⁻²∇λBh = im*kᵢ*∇⁻²∇λBh
+
+    ldiv!(∂ᵢ∇⁻²∇λB, grid.rfftplan,∇⁻²∇λBh)
+
+    @. ∂ᵢ∇⁻²∇λB = bᵢ*∂ᵢ∇⁻²∇λB
+
+    mul!(Bᵢ∂ᵢ∇⁻²∇λBh, grid.rfftplan, ∂ᵢ∇⁻²∇λB)
+
+    B∇∇⁻²∇λBh += Bᵢ∂ᵢ∇⁻²∇λBh
+
+  end
+
+  return nothing 
+
 end
 
-function LaplaceSolver(B,grid)
-    #=
-    funtion of computing ΔΦ = B using the fourier method, must be peroidic condition
-    Considering in k-space, k² Φ' = B', we would get Φ = F(B'/k²)
-    =#
-    k⁻² = grid.invKrsq;
-    T   = eltype(grid);
-    nx,ny,nz = size(B);
-    Φ    = zeros(T,nx,ny,nz);
-    Bh   = zeros(ComplexF32,(div(nx,2)+1,ny,nz));
-    mul!(Bh, grid.rfftplan, B); 
-    @. Bh*=k⁻²;
-    ldiv!(Φ, grid.rfftplan, deepcopy(Bh));
-    return Φ;
+# define the vars..
+function Get_the_∂ᵢD₁ᵢⱼ∂ⱼλₙ_term!(∂ᵢD₁ᵢⱼ∂ⱼλₙh, λh, D₁ᵢⱼ, kᵢ, kⱼ, params, vars, grid)
+  # 2 real and 2 imag sketch array
+  ∂ⱼλₙh = D₁ᵢⱼ∂ⱼλₙh = vars.nonlinh1  
+  ∂ⱼλₙ  = D₁ᵢⱼ∂ⱼλₙ = vars.nonlin1
+
+  @. ∂ⱼλₙh = im*kⱼ*λh
+
+  ldiv!(∂ⱼλₙ, grid.rfftplan, ∂ⱼλₙh)
+
+  D₁ᵢⱼ∂ⱼλₙ = @. D₁ᵢⱼ*∂ⱼλₙ
+
+  mul!(D₁ᵢⱼ∂ⱼλₙh, grid.rfftplan, D₁ᵢⱼ∂ⱼλₙ)
+
+  @. ∂ᵢD₁ᵢⱼ∂ⱼλₙh += im*kᵢ*D₁ᵢⱼ∂ⱼλₙh
+
+  return nothing 
+
 end
 
-function Crossproduct(A1,A2,A3,B1,B2,B3)
-  C1 = @.  (A2*B3 - A3*B2);
-  C2 = @. -(A1*B3 - A3*B1); 
-  C3 = @.  (A1*B2 - A2*B1);
-  return C1,C2,C3
-end
+# Compute the δA and add it back to B
+function ComputeδB!(λ, Ax, Ay, Az,  
+                    bx, by, bz, bxh, byh, bzh,
+                    vars, params, grid; 
+                    λBxh = sk1, λByh = sk2, λBzh = sk3,
+                    λAxh = sk4, λAyh = sk5, λAzh = sk6,
+                    δAxh = sk7, δAyh = sk8, δAzh = sk9)
+  
+  # define the sketch variables
+  λbᵢ = vars.nonlin1
+  δAxh, δAyh, δAzh = λBxh ,λByh ,λBzh
 
-#Define the synatex sugar for crossproduct and dot product
-(a1,a2,a3)×(b1,b2,b3) = Crossproduct(a1,a2,a3,b1,b2,b3);
-(a1,a2,a3)⋅(b1,b2,b3) = Dotproduct(a1,a2,a3,b1,b2,b3); 
+  kx,ky,kz = grid.kr, grid.l, grid.m
+  for (bᵢ, λbᵢh, Aᵢ, λAᵢh) ∈ zip((bx, by, bz), (λBxh ,λByh ,λBzh), (Ax, Ay, Az), (λAxh ,λAyh ,λAzh))
+    @. λbᵢ = λ*bᵢ
+    mul!(λbᵢh, grid.rfftplan, λbᵢ)
+    @. λbᵢ = λ*Aᵢ
+    mul!(λAᵢh, grid.rfftplan, λbᵢ)       
+  end
 
-function Dotproduct(A1,A2,A3,B1,B2,B3)
-    return A1.*B1 + A2.*B2 + A3.*B3 
+  # eq. 28 : δA = ∇×(λA) + λB - ∇∇⁻²∇⋅λB 
+  # since ∇×(∇ϕ) = 0 in the computation of B part, we neglect the ∇∇⁻²∇⋅λB part 
+  @. δAxh = im*(ky.*λAzh .- kz.*λAyh) + λBxh
+  @. δAyh = im*(kz.*λAxh .- kx.*λAzh) + λByh
+  @. δAzh = im*(kx.*λAyh .- ky.*λAxh) + λBzh
+
+  # B = B + ∇×(δA)
+  @. bxh += im*(ky*δAzh - kz*δAyh)
+  @. byh += im*(kz*δAxh - kx*δAzh)
+  @. bzh += im*(kx*δAyh - ky*δAxh)
+
+  return nothing
+  
 end

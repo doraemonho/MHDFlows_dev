72450d17c6e3d34e1879e744e52317b5e57c7a4a
woodyho13@gmail.com <HOphy!19>
2023-06-01 11:21:51 -0600

    debug for EMHDvars

--- src/Structure/datastructure.jl
+++ src/Structure/datastructure.jl
@@ -27,7 +27,7 @@ end
 function SetEMHDVars(::Dev, grid, usr_vars) where Dev
   T = eltype(grid)
     
-  @devzeros Dev T (grid.nx, grid.ny, grid.nz) bx  by bz ∇XBx ∇XBy ∇XBz ∇XBXB₁h ∇XBXB₂h ∇XBXB₃h nonlin1
+  @devzeros Dev T (grid.nx, grid.ny, grid.nz) bx  by bz ∇XBx ∇XBy ∇XBz ∇XBXB₁ ∇XBXB₂ ∇XBXB₃ nonlin1
   @devzeros Dev Complex{T} (grid.nkr, grid.nl, grid.nm)  nonlinh1 ∇XBxh ∇XByh ∇XBzh ∇XBXB₁h ∇XBXB₂h ∇XBXB₃h
   
   return EMVars(bx,  by,  bz,
